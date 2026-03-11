186cf749ee8d792e8b4da27d9b71da6ecccd03a4
Remi Soitel <remi.soitel@etudiant.univ-reims.fr>
2026-03-11 14:15:03 +0100

    Test erreur

--- app/main.py
+++ app/main.py
@@ -21,7 +21,7 @@ def get_db():
 # Schémas Pydantic pour les requêtes et réponses
 class TaskCreate(BaseModel):
     title: str
-    description: Optional[str] = None
+    description: Optional[str]
 
 class TaskUpdate(BaseModel):
     title: Optional[str] = None
